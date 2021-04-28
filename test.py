d538e644dc715bbb2d68fd4a205c3570f30f246d
gkaln <55789275+gkaln@users.noreply.github.com>
2021-04-28 14:18:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Andreas Goulas <goulasand@gmail.com>
+# Andreas Goulas <goulasand@gmail.com> | first creation
 # Nikolaos Gkalelis <gkalelis@iti.gr> | 23/4/2021 | minor changes (text processing, etc.)
 
 import argparse
